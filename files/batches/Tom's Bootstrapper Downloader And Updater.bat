7c05ec7804185f82f6e4ee8ba82d46d0bdd29d90
tomtep01 <102281952+tomtep01@users.noreply.github.com>
2022-09-21 18:08:18 +0700

    Downloader and Updater

---
+++ files/batches/Tom's Bootstrapper Downloader And Updater.bat
@@ -0,0 +1,29 @@
+@echo off
+echo If files doesn't appear, don't run as an administrator. Downloading in 3 seconds
+timeout /t 3 /nobreak >nul
+echo Downloading 7z.exe...
+curl https://raw.githubusercontent.com/tomtep01/Tom-s-Bootstrapper-Files/main/7z.exe > 7z.exe
+if %errorlevel%==0  (
+echo Success!
+) else (
+echo Downloading failed, skipping to next file (please try again later)
+)
+echo Downloading main files...
+curl https://raw.githubusercontent.com/tomtep01/Tom-s-Bootstrapper-Files/main/temp.zip > temp.zip
+if %errorlevel%==0  (
+echo Success!
+) else (
+echo Failed!
+)
+echo Extracting (if its not failed)
+7z e -aoa temp.zip >nul
+if %errorlevel%==0  (
+echo Extracted!
+) else (
+echo Failed!
+)
+del 7z.exe
+del temp.zip
+echo Quitting in 4 seconds... Enjoy!
+timeout /t 4 /nobreak >nul
+exit
